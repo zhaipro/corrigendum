5985109807f3745fd259f6b0c4994a79d5e9b2ee
zhai_pro <xin_soft@foxmail.com>
2015-05-05 15:44:40 +0800

    增加static目录
    决定前端使用uikit框架

--- corrigendum/corrigendum/settings.py
+++ corrigendum/corrigendum/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+# The corrigendum app
     'main',
 )
 
@@ -101,3 +102,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+# The static root
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
