09536971da25f0d17468ca6d9005623769e226ac
zhai_pro <xin_soft@foxmail.com>
2015-05-06 00:02:10 +0800

    增加markdown编辑器

--- corrigendum/migrate.bat
+++ corrigendum/migrate.bat
@@ -2,5 +2,5 @@ rmdir /S /Q main\migrations
 rm db.sqlite3
 manage.py makemigrations main
 manage.py migrate
-manage.py createsuperuser
+manage.py createsuperuser --username admin --email xin_soft@foxmail.com
 manage.py runserver
